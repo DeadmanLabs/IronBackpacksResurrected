6a649a08a499b2f0d6f5ae9919a40b330ccc55b8
DeadmanLabs <michael.magahey@gmail.com>
2024-11-25 11:03:17 -0500

    texture stuff and basic organization

---
+++ src/main/java/com/iron/ironbackpacks/item/BackpackBasicItem.java
@@ -0,0 +1,10 @@
+package com.iron.ironbackpacks.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+
+public class BackpackBasicItem extends Item {
+    public BackpackBasicItem(Properties properties) {
+        super(properties);
+    }
+}
